ab02e466b09ee2a20b3310105ddd092a4557a1f5
yong cheng low <lowyongcheng@hotmail.com>
2023-02-11 23:25:57 +0800

    Config: Remove docs

--- docs/_next/static/chunks/pages/_scripts-106d555a5b7b28c8.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[38],{6384:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_scripts",function(){return u(9660)}])}},function(n){n.O(0,[774,888,179],function(){return n(n.s=6384)}),_N_E=n.O()}]);
